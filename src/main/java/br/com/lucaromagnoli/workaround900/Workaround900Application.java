1a9a03eb2eea3cf550f81e9b57134c840ca5736a
Lucas Romagnoli <lucasr.romagnoli@gmail.com>
2023-01-26 21:16:50 -0300

    iniciando pocs com webflux e websocket

---
+++ src/main/java/br/com/lucaromagnoli/workaround900/Workaround900Application.java
@@ -0,0 +1,13 @@
+package br.com.lucaromagnoli.workaround900;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workaround900Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Workaround900Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lucaromagnoli/workaround900/Workaround900ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucaromagnoli.workaround900;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workaround900ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
